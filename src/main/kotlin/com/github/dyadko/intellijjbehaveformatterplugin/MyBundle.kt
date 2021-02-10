e012ea023812b812b290a8d42d28d493fb437441
GitHub Action <action@github.com>
2021-02-10 19:51:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/dyadko/intellijjbehaveformatterplugin/MyBundle.kt
+++ src/main/kotlin/com/github/dyadko/intellijjbehaveformatterplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dyadko.intellijjbehaveformatterplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/dyadko/intellijjbehaveformatterplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dyadko.intellijjbehaveformatterplugin.services
+
+import com.github.dyadko.intellijjbehaveformatterplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
